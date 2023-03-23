1eacc4648d16c20e0a0f5abd3f92ac98b6b943dd
David Aguirre <david.aguirre@epn.edu.ec>
2023-03-23 02:12:10 -0500

    [ADD] [DA] about me y footer creacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContenidoPrincipalComponent } from './components/contenido-principal/contenido-principal.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'main-content',
+    component: ContenidoPrincipalComponent,
+    children:[
+      {
+        path:'about-me',
+        component:SobreMiComponent
+      }
+    ]
+  },
+  {
+    path:'',
+    redirectTo:'/main-content/about-me',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ContenidoPrincipalComponent } from './components/contenido-principal/contenido-principal.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    ContenidoPrincipalComponent,
+    SobreMiComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
