e64e0c8d9ec30ac0bd97769bf34d974405949856
David Aguirre <david.aguirre@epn.edu.ec>
2023-04-28 22:50:52 -0500

    [ADD] [DA] Responsive footer y skills

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -40,6 +40,32 @@ export class ExperienciaComponent {
         'Desarrollo frontend'
       ]
     },
+    {
+      imagen : '../../../assets/img/logoepn.png',
+      cargo : "Miembro Evaluador de Mallas Curriculares",
+      empresa: "Escuela Politécnica Nacional",
+      periodo: "Enero 2023 - Marzo 2023",
+      actividades:[
+        'Evaluar mallas curriculares de la Facultad de Ingeniería de Sistemas',
+        'Participar en debates referentes aspectos de las materias de la mallas curriculares',
+        'Proponer soluciones de mejora de contenidos de mallas curriculares',
+        'Evaluar recursos disponibles para maestros y estudiantes',
+        'Calificar métodos de enseñanza-aprendizaje utilizados'
+      ]
+    },
+    {
+      imagen : '../../../assets/img/Banco-Pichincha.png',
+      cargo : "Gestor de Contratos Tecnológicos",
+      empresa: "Banco Pichincha",
+      periodo: "Abril 2023 - Septiembre 2023",
+      actividades:[
+        'Desbloquear flujos de contratos tecnológicos',
+        'Analizar garantías de proveedores',
+        'Actualizar matriz de flujos de contratos',
+        'Monitorear avances en los flujos de contratos',
+        'Actualizar reportes en Power BI'
+      ]
+    },
   ]
 
 
